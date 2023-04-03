0565e5b756dcb3e0019fce491eded9691bc4db1e
canknbr <cankanbur1@gmail.com>
2023-04-03 17:50:31 +0300

    Initial Commit

---
+++ Swing Animations/Swing_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swing_AnimationsApp.swift
+//  Swing Animations
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swing_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
